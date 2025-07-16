883017583dfcaa126c982597c25c483ac4dc5765
Niveshavc <nivesheceece@gmail.com>
2025-07-16 14:38:30 +0530

    add:Suceesfully add and read data from textSync.txt

--- App.js
+++ App.js
@@ -1,3 +1,7 @@
 const fs=require('fs')
 fs.writeFileSync('textSync.txt','iam writing a new file')
 console.log('File written sucessfully')
+
+
+const data=fs.readFileSync('textSync.txt',{encoding:'utf-8'})
+console.log('SuccesFully read the file',data)
